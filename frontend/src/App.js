6b2014ee28f9a4622f183a6663b563b73bbcc660
Arnau Bertrans <arnaubertrans@gmail.com>
2025-01-03 21:54:00 +0100

    1st api request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
+
 const App = () => {
   // usestate retorna 2 variables, str(word) i function(setWord)
   // '' variable inicial del state
@@ -10,11 +12,21 @@ const App = () => {
   
   const handleSearchSubmit = (e) => {
     e.preventDefault()
+    // amb word controlem cada caràcter q l'user introdueix a l'input
     console.log(word)
-  }
 
-  // amb word controlem cada caràcter q l'user introdueix a l'input
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }  
   // console.log(word)
+
+
 
   return (
     <div>
