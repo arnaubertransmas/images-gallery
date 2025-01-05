361c46e2ce50bb1d4a79a32508b616dfb1784def
Arnau Bertrans <arnaubertrans@gmail.com>
2025-01-05 23:59:20 +0100

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
@@ -11,31 +13,41 @@ const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
 
-  console.log(images);
-
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     // amb word controlem cada caràcter q l'user introdueix a l'input
-    console.log(word);
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
     )
       .then((result) => result.json())
       .then((data) => {
         // afegim la nova img al principi i fem un retrive de les images antigues
-        setImages([data, ...images]); // no fem push pq així evitem mutar la llista
+        setImages([{ ...data, title: word }, ...images]); // no fem push pq així evitem mutar la llista
       })
       .catch((error) => {
         console.log(error);
       });
     setWord('');
   };
-  // console.log(word)
+
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
+  };
 
   return (
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={3} ld={3}>
+          {/* loop x l'array d'images i les va mostrant | key per afegir identificador únic*/}
+          {images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
